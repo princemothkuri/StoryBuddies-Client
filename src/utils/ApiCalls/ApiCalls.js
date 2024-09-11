5ed4fbaf9db0b64544870d01fcb1817b23c39e4e
princemothkuri <princemothkuri@gmail.com>
2024-09-11 19:22:49 +0530

    changed server domain

--- src/utils/ApiCalls/ApiCalls.js
+++ src/utils/ApiCalls/ApiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://storybuddies-server.onrender.com";
 
 // User Authentication
 export const register = (userData) =>
